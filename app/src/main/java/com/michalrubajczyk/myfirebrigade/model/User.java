7c8c8213683bfc97557e8e6707ae8d5584f499c8
mrubajczyk <rubajticos@gmail.com>
2018-02-27 16:02:11 +0100

    added 2 parametrized constructor

--- app/src/main/java/com/michalrubajczyk/myfirebrigade/model/User.java
+++ app/src/main/java/com/michalrubajczyk/myfirebrigade/model/User.java
@@ -14,6 +14,11 @@ public class User {
     public User() {
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public int getId() {
         return id;
     }
