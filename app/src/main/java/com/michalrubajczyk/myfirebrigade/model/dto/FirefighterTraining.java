a9d409708874e2c36445154c6e1fff596ce9327d
mrubajczyk <rubajticos@gmail.com>
2018-04-20 12:37:09 +0200

    uzupełniono klasę FirefighterTraining

--- app/src/main/java/com/michalrubajczyk/myfirebrigade/model/dto/FirefighterTraining.java
+++ app/src/main/java/com/michalrubajczyk/myfirebrigade/model/dto/FirefighterTraining.java
@@ -1,4 +1,44 @@
 package com.michalrubajczyk.myfirebrigade.model.dto;
 
+import java.util.Date;
+
 public class FirefighterTraining {
+
+    private int idFirefighterTraining;
+    private Firefighter firefighter;
+    private Training training;
+    private Date trainingDate;
+
+    public int getIdFirefighterTraining() {
+        return idFirefighterTraining;
+    }
+
+    public void setIdFirefighterTraining(int idFirefighterTraining) {
+        this.idFirefighterTraining = idFirefighterTraining;
+    }
+
+    public Firefighter getFirefighter() {
+        return firefighter;
+    }
+
+    public void setFirefighter(Firefighter firefighter) {
+        this.firefighter = firefighter;
+    }
+
+    public Training getTraining() {
+        return training;
+    }
+
+    public void setTraining(Training training) {
+        this.training = training;
+    }
+
+    public Date getTrainingDate() {
+        return trainingDate;
+    }
+
+    public void setTrainingDate(Date trainingDate) {
+        this.trainingDate = trainingDate;
+    }
+
 }
