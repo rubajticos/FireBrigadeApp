7797b85b6607f96581919832ea7e05b4caa61bdc
mrubajczyk <rubajticos@gmail.com>
2018-04-20 12:36:06 +0200

    dodano klasę dotyczącą umiejętności strażaka

---
+++ app/src/main/java/com/michalrubajczyk/myfirebrigade/model/dto/Training.java
@@ -0,0 +1,23 @@
+package com.michalrubajczyk.myfirebrigade.model.dto;
+
+public class Training {
+
+    private int idTraining;
+    private String name;
+
+    public int getIdTraining() {
+        return idTraining;
+    }
+
+    public void setIdTraining(int idTraining) {
+        this.idTraining = idTraining;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
