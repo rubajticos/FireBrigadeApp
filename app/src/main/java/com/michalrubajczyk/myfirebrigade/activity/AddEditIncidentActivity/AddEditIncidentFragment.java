3bf387238abfc0f1928032eed92c0b7eb0c2f634
mrubajczyk <rubajticos@gmail.com>
2018-05-24 12:59:49 +0200

    dodane kolejne dynamiczne elementy dla layoutu zdarzeń: - typ i podtyp zdarzenia - wybor ratownikow i sprzetu
    Signed-off-by: mrubajczyk <rubajticos@gmail.com>

--- app/src/main/java/com/michalrubajczyk/myfirebrigade/activity/AddEditIncidentActivity/AddEditIncidentFragment.java
+++ app/src/main/java/com/michalrubajczyk/myfirebrigade/activity/AddEditIncidentActivity/AddEditIncidentFragment.java
@@ -1,5 +1,6 @@
 package com.michalrubajczyk.myfirebrigade.activity.AddEditIncidentActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.graphics.Typeface;
@@ -8,9 +9,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -23,6 +26,7 @@ import android.widget.TimePicker;
 import com.michalrubajczyk.myfirebrigade.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Map;
@@ -47,6 +51,12 @@ public class AddEditIncidentFragment extends Fragment implements AddEditIncident
     @BindView(R.id.add_edit_incident_datetime_of_alarm)
     TextView mDatetimeOfAlarm;
 
+    @BindView(R.id.add_edit_incident_type)
+    Spinner mIncidentType;
+
+    @BindView(R.id.add_edit_incident_subtype)
+    Spinner mIncidentSubtype;
+
     @BindView(R.id.add_edit_incident_details)
     LinearLayout mDetailsLayout;
 
@@ -58,6 +68,7 @@ public class AddEditIncidentFragment extends Fragment implements AddEditIncident
 
     Unbinder unbinder;
 
+
     private AddEditIncidentContract.Presenter mPresenter;
 
     private SimpleDateFormat dateFormatter;
@@ -65,6 +76,13 @@ public class AddEditIncidentFragment extends Fragment implements AddEditIncident
 
     private Calendar mCalendar;
 
+    String[] firefighterItems = new String[]{"Michał Rubajczyk", "Krzysztof Moskal", "Michał Moskal", "Łukasz Gomółka", "Mateusz Patryn", "Test", "tererere"};
+    boolean[] firefighterCheckedItems;
+    ArrayList<Integer> mFirefighterItems = new ArrayList<>();
+
+    String[] eqItems = new String[]{"Hooligan", "Wąż W-52"};
+    boolean[] equipmentCheckedItems;
+    ArrayList<Integer> mEquipmentItems = new ArrayList<>();
 
     public AddEditIncidentFragment() {
         this.dateFormatter = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
@@ -100,8 +118,49 @@ public class AddEditIncidentFragment extends Fragment implements AddEditIncident
             createDateTimePickerDialogAndSetDateTime(mDatetimeOfAlarm);
         });
 
+        String[] mainCategories = getResources().getStringArray(R.array.incident_main_categories);
+        ArrayAdapter<String> mainTypeAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, mainCategories);
+        mainTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        String[] mainFirstValueSubcategories = getResources().getStringArray(R.array.incident_pozar_subcategories);
+        String[] mainSecondValueSubcategories = getResources().getStringArray(R.array.incident_miejscowe_subcategories);
+        String[] mainThirdValueSubcategories = getResources().getStringArray(R.array.incident_cwiczenia_subcategories);
+        String[] mainFourthValueSubcategories = getResources().getStringArray(R.array.incident_falszywe_subcategories);
+
+        mIncidentType.setAdapter(mainTypeAdapter);
+        mIncidentType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        setSubcategoriesAdapter(mainFirstValueSubcategories);
+                        break;
+                    case 1:
+                        setSubcategoriesAdapter(mainSecondValueSubcategories);
+                        break;
+                    case 2:
+                        setSubcategoriesAdapter(mainThirdValueSubcategories);
+                        break;
+                    case 3:
+                        setSubcategoriesAdapter(mainFourthValueSubcategories);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        setSubcategoriesAdapter(mainFirstValueSubcategories);
+
         mAddCarButton.setOnClickListener(view -> createIncidentCarLayout());
         return root;
+    }
+
+    private void setSubcategoriesAdapter(String[] subcategoriesArray) {
+        ArrayAdapter<String> subtypeAdapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, subcategoriesArray);
+        subtypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mIncidentSubtype.setAdapter(subtypeAdapter);
     }
 
     private void createDatePickerDialogAndSetDate(TextView mDateTV) {
@@ -153,27 +212,115 @@ public class AddEditIncidentFragment extends Fragment implements AddEditIncident
         String[] commandors = new String[]{"Zdzisław Moskal", "Piotr Pawłowski"}; //tymczasowo
         String[] drivers = new String[]{"Paweł Pukacz", "Tomasz Szczygieł"}; //tymczasowo
 
+        //CAR section
         TextView carLabel = createLabelTextView(getResources().getString(R.string.add_edit_incident_car));
         Spinner carSpinner = createSpinner(cars);
         singleCarLL.addView(carLabel);
         singleCarLL.addView(carSpinner);
 
+        //DATE OF DEPARTURE section
+//        TextView
+
+        //DATE OF RETURN section
+
+        //COMMANDER section
         TextView commandorLabel = createLabelTextView(getResources().getString(R.string.add_edit_incident_commander));
         Spinner commandorsSpinner = createSpinner(commandors);
         singleCarLL.addView(commandorLabel);
         singleCarLL.addView(commandorsSpinner);
 
+        //DRIVER section
         TextView driversLabel = createLabelTextView(getResources().getString(R.string.add_edit_incident_driver));
         Spinner driversSpinner = createSpinner(drivers);
         singleCarLL.addView(driversLabel);
         singleCarLL.addView(driversSpinner);
 
+        //FIREFIGHTERS section
+        TextView firefightersLabel = createLabelTextView("Ratownicy");
+        Button chooseFirefightersButton = new Button(getActivity());
+        chooseFirefightersButton.setText("Wybierz ratowników");
+        TextView firefightersTV = createSimpleTextView("Wybrani ratownicy...");
+
+        firefighterCheckedItems = new boolean[firefighterItems.length];
+        chooseFirefightersButton.setOnClickListener(view -> {
+            createMultichoiceDialogWithData(firefighterItems, firefighterCheckedItems, mFirefighterItems, firefightersTV);
+        });
+
+        singleCarLL.addView(firefightersLabel);
+        singleCarLL.addView(chooseFirefightersButton);
+        singleCarLL.addView(firefightersTV);
+
+        //EQUIPMENT section
+        TextView equipmentLabel = createLabelTextView("Użyty sprzęt");
+        Button chooseEquipmentButton = new Button(getActivity());
+        chooseEquipmentButton.setText("Wybierz użyty sprzęt");
+        TextView equipmentsTV = createSimpleTextView("Wybrany sprzęt");
+
+        equipmentCheckedItems = new boolean[eqItems.length];
+        chooseEquipmentButton.setOnClickListener(view -> {
+            createMultichoiceDialogWithData(eqItems, equipmentCheckedItems, mFirefighterItems, equipmentsTV);
+        });
+
+        singleCarLL.addView(equipmentLabel);
+        singleCarLL.addView(chooseEquipmentButton);
+        singleCarLL.addView(equipmentsTV);
+
+        //END SECTION
         View spacer = createLineSpacer();
         singleCarLL.addView(spacer);
 
         mDetailsLayout.addView(singleCarLL);
 
         // TODO: 24/05/2018 data powrotu i wyjazdu, ratownicy, sprzet
+    }
+
+    private TextView createSimpleTextView(String text) {
+        TextView textView = new TextView(getActivity());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 10, 0, 10);
+        textView.setLayoutParams(lp);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        textView.setText(text);
+        textView.setVisibility(View.VISIBLE);
+        return textView;
+    }
+
+    private void createMultichoiceDialogWithData(String[] items, boolean[] checkedItems, ArrayList<Integer> mItems, TextView resultTv) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+        mBuilder.setTitle("Wybierz ratowników");
+        mBuilder.setMultiChoiceItems(items, checkedItems, (dialogInterface, position, isChecked) -> {
+            if (isChecked) {
+                mItems.add(position);
+            } else if (mItems.contains(position)) {
+                mItems.remove(Integer.valueOf(position));
+            }
+        });
+
+        mBuilder.setCancelable(false);
+        mBuilder.setPositiveButton("OK", (dialogInterface, which) -> {
+            String item = "";
+            for (int i = 0; i < mItems.size(); i++) {
+                item = item + items[mItems.get(i)];
+                if (i != mItems.size() - 1) {
+                    item = item + ", ";
+                }
+            }
+            resultTv.setText(item);
+        });
+
+        mBuilder.setNegativeButton("Anuluj", (dialogInterface, i) -> dialogInterface.dismiss());
+
+        mBuilder.setNeutralButton("Wyczyść", (dialogInterface, which) -> {
+            for (int i = 0; i < checkedItems.length; i++) {
+                checkedItems[i] = false;
+                mItems.clear();
+                resultTv.setText("");
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+
     }
 
     private View createLineSpacer() {
